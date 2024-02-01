dc66b278f7462aebdae951e7661b14edc234f4ea
yidong4510 <153195008+yidong4510@users.noreply.github.com>
2024-02-01 20:27:24 +0900

    ユーザー管理機能

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :password, presence: true, length: {minimum: 6 }, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+         validates :email, presence: true, uniqueness: true
+         validates :last_name, presence: true
+         validates :first_name, presence: true
+         validates :kana_last_name, presence: true
+         validates :kana_first_name, presence: true
+         validates :birthday, presence: true
+end
