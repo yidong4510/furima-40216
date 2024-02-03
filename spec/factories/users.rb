92564a0a57ab5f20184f51114200e30603131bb6
yidong4510 <153195008+yidong4510@users.noreply.github.com>
2024-02-03 14:59:09 +0900

    モデル単体テスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {Faker::Japanese::Name.first_name}
+    first_name            {Faker::Japanese::Name.last_name}
+    kana_last_name        {Faker::Japanese::Name.last_name.yomi}
+    kana_first_name       {Faker::Japanese::Name.last_name.yomi}
+    birthday              {Faker::Date.birthday}
+  end
+end
