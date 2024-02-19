e25d72d00fc4cf70377e56205f7e6ff3a3088d98
yidong4510 <153195008+yidong4510@users.noreply.github.com>
2024-02-19 21:37:48 +0900

    javascript実装

---
+++ app/javascript/controllers/item_price.js
@@ -0,0 +1,18 @@
+function priceCalc () {
+  const priceInput = document.getElementById("item-price");
+  const commission = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+  priceInput.addEventListener("input",() => {
+    const inputValue = priceInput.value;
+    if (inputValue < 300 ){
+      commission.innerHTML = 0
+      profit.innerHTML = 0
+    } else {
+      const commissionValue = Math.floor(inputValue * 0.1);
+      commission.innerHTML = commissionValue;
+      profit.innerHTML = (inputValue - commissionValue);  
+    }
+  });
+};
+
+window.addEventListener('turbo:load', priceCalc);
