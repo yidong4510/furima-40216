4b73411b28c57f5fcaa110e33d3f3d86bc85d1f5
yidong4510 <153195008+yidong4510@users.noreply.github.com>
2024-02-21 20:50:04 +0900

    モデル単体テスト修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -23,27 +23,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it 'category_idが空だと登録できない' do
-        @item.category_id = '' || @item.category_id = 1
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it 'shipping_charge_idが空だと登録できない' do
-        @item.shipping_charge_id = '' ||  @item.shipping_charge_id = 1
+        @item.shipping_charge_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping charge can't be blank")
       end
       it 'prefecture_idが空だと登録できない' do
-        @item.prefecture_id = '' || @item.prefecture_id = 1
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it 'condition_idが空だと登録できない' do
-        @item.condition_id = '' || @item.condition_id = 1
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it 'day_idが空だと登録できない' do
-        @item.day_id = '' || @item.day_id = 1
+        @item.day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Day can't be blank")
       end
