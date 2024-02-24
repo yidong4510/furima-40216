bc8145e7c6e4814a7188332154adba99216e01d9
yidong4510 <153195008+yidong4510@users.noreply.github.com>
2024-02-24 18:47:44 +0900

    これいる？

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :delete]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit, :destroy]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
