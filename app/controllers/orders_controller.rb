1b1588cb7accef26147ce38c3bf8736b344d3ea0
yidong4510 <153195008+yidong4510@users.noreply.github.com>
2024-03-12 21:14:49 +0900

    order　viewの調整

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,7 @@ class OrdersController < ApplicationController
   
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
