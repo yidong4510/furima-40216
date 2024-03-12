fea509150427e482431810f7a54bc19ec59210f8
yidong4510 <153195008+yidong4510@users.noreply.github.com>
2024-03-12 21:09:33 +0900

    クレジット決済

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,36 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path 
+    else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      render :index, status: :unprocessable_entity
+    end 
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :block, :building, :tel, :order_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,5 @@ class User < ApplicationRecord
   validates :birthday, presence: true
 
   has_many :items
+  has_one :order
 end
